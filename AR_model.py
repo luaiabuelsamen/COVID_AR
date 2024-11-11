ddc308594d569027dccbfa60c362bace682b774c
labuelsamen <luai.abuelsamen@mail.mcgill.ca>
2024-11-11 02:51:01 -0800

    changing to flask

--- AR_model.py
+++ AR_model.py
@@ -1,185 +1,104 @@
-"""
-MECH 309
-Data from
-https://www.donneesquebec.ca/recherche/dataset/covid-19-portrait-quotidien-des-cas-confirmes
-Sample code for students
-"""
-from doctest import ELLIPSIS
+from flask import Flask, render_template, jsonify
+import yfinance as yf
 import numpy as np
 from scipy import linalg
-from matplotlib import pyplot as plt
+import pandas as pd
+from datetime import datetime, timedelta
 
-# %% Plotting parameters
-
-# plt.rc('text', usetex=True)
-plt.rc('font', family='serif', size=16)
-plt.rc('lines', linewidth=2)
-plt.rc('axes', grid=True)
-plt.rc('grid', linestyle='--')
-
-# %% Load data
-
-data = np.loadtxt('./COVID19QC.csv', dtype=float,delimiter=',', skiprows=2, usecols=(2,))
-
-y = data.copy()
-
-# Plot the raw COVID-19 data, new cases per day versus time
-N = data.size
-
-t = np.linspace(0, N, N, endpoint=True)
-
-fig, ax = plt.subplots()
-
-fig.set_size_inches(18.5, 10.5, forward=True)
-ax.set_title(r'New COVID-19 Cases per Day versus Time')
-ax.set_xlabel(r'$t$ (days)')
-
-ax.set_ylabel(r'$y_k$ (cases)')
-
-plt.plot(t, y, label='New Cases per Day')
-
-ax. legend(loc='center left', bbox_to_anchor=(1, 10.5))
-fig.savefig('COVID19_cases_vs_time.pdf')
-
-# %% AR model functions
-
-#print (data)
+app = Flask(__name__)
 
 def form_A_b(y, ell):
     N = y.size
     A = np.ones((N - ell, ell))
     for n in range(N - ell):
-        A[n] = y[n : ell +n]
+        A[n] = y[n : ell + n]
     b = y[ell : N]
     return A, b
 
 def fit(A, b):
-    # This function solves Ax = b where x are the AR model parameters
     return linalg.solve(A.T @ A, A.T @ b)
 
 def predict(y, beta):
     N = y.size
     ell = beta.size
-    y_pred =y * 0
+    y_pred = np.zeros_like(y)
     for n in range(ell, N):
         y_pred[n] = beta.reshape(1, -1) @ y[n - ell : n].reshape(-1, 1)
-        # Set the first ell predictions to the average of the first ell measurments
-        y_pred_mean = np.mean(y[:ell]) # Don't change this
-        y_pred[:ell] = np.ones(ell) * y_pred_mean # Don't change this
-        return y_pred
+    # Set the first ell predictions to the average of the first ell measurements
+    y_pred_mean = np.mean(y[:ell])
+    y_pred[:ell] = np.ones(ell) * y_pred_mean
+    return y_pred
 
-# %% Fit (train) the AR model
+def get_stock_data(symbol, period='1y'):
+    stock = yf.Ticker(symbol)
+    df = stock.history(period=period)
+    return df['Close'].values
 
-N_start = 49 # start day, don't change
-N_end = 399 # end day, don't change
+def train_model(data, ell=16):
+    y_scale = np.max(data)
+    y_normalized = data / y_scale
+    
+    A, b = form_A_b(y_normalized, ell)
+    beta = fit(A, b)
+    
+    return beta, y_scale
 
-ell = 16 # memory of AR model, change to 14, 15, 16
-t = np.arange(N_start, N_end) # time, don't change
-y_scale = np.max(y[N_start:N_end])
+def make_prediction(data, beta, y_scale, future_days=30):
+    # Normalize the data
+    y_normalized = data / y_scale
+    
+    # Make prediction for existing data
+    y_pred = predict(y_normalized, beta)
+    
+    # Scale predictions back
+    y_pred = y_pred * y_scale
+    
+    # Generate future predictions
+    future_pred = []
+    last_values = data[-beta.size:]
+    
+    for _ in range(future_days):
+        next_pred = (beta.reshape(1, -1) @ (last_values[-beta.size:] / y_scale).reshape(-1, 1))[0] * y_scale
+        future_pred.append(float(next_pred))
+        last_values = np.append(last_values[1:], next_pred)
+    
+    return y_pred, future_pred
 
-ly=y/y_scale # non-dimensionalize the data
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/predict/<symbol>')
+def predict_stock(symbol):
+    try:
+        # Get historical data
+        data = get_stock_data(symbol)
+        
+        # Train model
+        beta, y_scale = train_model(data)
+        
+        # Make predictions
+        historical_pred, future_pred = make_prediction(data, beta, y_scale)
+        
+        # Prepare dates
+        dates = [(datetime.now() - timedelta(days=len(data)-i)).strftime('%Y-%m-%d') 
+                for i in range(len(data))]
+        future_dates = [(datetime.now() + timedelta(days=i)).strftime('%Y-%m-%d') 
+                       for i in range(1, len(future_pred)+1)]
+        
+        return jsonify({
+            'success': True,
+            'historical_dates': dates,
+            'historical_actual': data.tolist(),
+            'historical_predicted': historical_pred.tolist(),
+            'future_dates': future_dates,
+            'future_predicted': future_pred
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        })
 
-A, b = form_A_b(y[N_start:N_end], ell) # form A, b matrices
-beta = fit(A, b) # find the beta parameters
-
-y=y*y_scale # dimensionalize the data again
-
-y_pred = predict(y[N_start:N_end], beta)
-
-e = np.abs(y[N_start:N_end] - y_pred)
-
-
-# Plotting
-fig, ax = plt.subplots(2, 1)
-
-fig.set_size_inches(18.5, 10.5, forward=True)
-
-ax[0].set_title(r'AR Model Train')
-
-for a in np.ravel(ax):
-    a.set_xlabel(r'$t$ (days)')
-    ax[0].set_ylabel(r'$y_k¢ (cases)')
-    ax[1].set_ylabel(r'$e_k$ (cases)')
-
-    ax[0].plot(t[ell:], y_pred[ell:], label=r'sy_{k, pred, \ell=%s}$' % ell)
-    ax[0].plot(t[ell:], y[N_start + ell:N_end], '--', label=r'$y {k, true}$')
-    ax[1].plot(t[ell:], e[ell:], label=r'$e_{k, \ell=%s}$' % ell)
-
-    ax[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    ax[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-fig.tight_layout()
-plt.show()
-
-fig. savefig('AR_response_train.pdf')
-
-#%% Teast
-
-y = data.copy()
-
-N_start = 400 # start day, don't change
-N_end = 625 # end day, don't change
-
-t = np.arange(N_start, N_end)
-
-y_pred = predict(y[N_start:N_end], beta)
-
-# predictions
-# Compute various metrics associated with predictino error
-
-e = np.abs(y[N_start:N_end] - y_pred)
-
-e_rel = np.abs(y[N_start:N_end] - y_pred)/y[N_start:N_end]
-
-mu = np.mean(e)
-
-sigma = np.std(e)
-
-mu_e_rel = np.mean(e_rel)
-sigma_e_rel = np.std(e_rel)
-
-fig, ax = plt.subplots(3, 1)
-# Format axes
-fig.set_size_inches(18.5, 10.5, forward=True)
-ax[0].set_title(r'AR Model Test')
-for a in np.ravel(ax):
-    a.set_xlabel(r'$t$ (days)')
-ax[0].set_ylabel(r'$y_k$ (cases)')
-ax[1].set_ylabel(r'$e_k$ (cases)')
-ax[1].hlines(y=mu, xmin=N_start, xmax=N_end, linewidth=2, color='r')
-ax[1].hlines(y=mu + 3*sigma, xmin=N_start, xmax=N_end, linewidth=2, color= 'r')
-ax[1].hlines(y=mu - 3*sigma, xmin=N_start, xmax=N_end, linewidth=2, color= 'r')
-ax[2].set_ylabel(r'$e_{k, rel}$ (%)')
-ax[2].hlines(y=mu_e_rel, xmin=N_start, xmax=N_end, linewidth=2, color='r')
-
- 
-
-  
-
-ax[2].hlines(y-mu_e_rel + 3*sigma_e_rel, xmin=N_start, xmax=N_end, linewidth=2,
-
-color='r')
-
-ax[2].hlines(y=mu_e_rel - 3*sigma_e_rel, xmin=N_start, xmax=N_end, linewidth=2,
-
-color='r')
-
-ax[0].plot(t[ell:], y_pred[ell:], label=r'$y_{k, pred, \ell=%s}$' % ell)
-ax[1].plot(t[ell:], e[ell:], label=r'$e_{k, \ell=%s}$' % ell)
-ax[0].plot(t[ell:], y[N_start + ell:N_end], '--', label=r'$y {k, true}$')
-ax[2].plot(t[ell:], e_rel[ell:], label=r'$e_{k, rel, \ell-%s}$' % ell)
-ax[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-ax[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-ax[2].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-fig.tight_layout()
-
-plt.show()
-
-fig.savefig('AR_response_test_ell_%s.pdf' % ell)
-
-print('Mean absolute error is ', mu, '\n')
-
-print('Absolute error standard deviation is', sigma, '\n')
-print('Mean relative error is ', mu_e_rel, '\n')
-print('Relative error standard deviation is', sigma_e_rel, '\n')
+if __name__ == '__main__':
+    app.run(debug=True)
